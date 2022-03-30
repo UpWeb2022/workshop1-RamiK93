a532be6e2de196a6b93eb544a329db98cc873e7f
RamiK93 <rami.kaabi@esprit.tn>
2022-03-30 20:35:10 +0200

    update 30-03

---
+++ src/app/list-product/list-product.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../model/product';
+
+@Component({
+  selector: 'app-list-product',
+  templateUrl: './list-product.component.html',
+  styleUrls: ['./list-product.component.css']
+})
+export class ListProductComponent implements OnInit {
+  public titleApp: string;
+  public list:Product[];
+  constructor() {
+
+   }
+
+  ngOnInit(): void {
+    this.titleApp= 'First angular App';
+    this.list= [
+      {id: 12,
+      title : 't-shirt',
+      descreption: '100% coton',
+      price: 120,
+      quantity: 0,
+      picture: 'https://contents.mediadecathlon.com/p1901252/k$5b79ad35576fa8adc7330b6c770fc6e3/t-shirt-sportee-100-coton-homme-noir.jpg?&f=452x452',
+      like: 25},
+      {id: 32,
+        title : 'djean',
+        descreption: '100% djean',
+        price: 150,
+        quantity: 63,
+        picture: 'https://pochettesquareplus.tn/wp-content/uploads/2021/06/8A3C2E22-5F6B-46CF-973B-04C55EEBC38E.jpeg',
+        like: 29},
+        {id: 2,
+          title : 'chemise',
+          descreption: '100% djean',
+          price: 10,
+          quantity: 93,
+          picture: 'https://photos6.spartoo.com/photos/188/18830844/18830844_500_B.jpg',
+          like: 2}
+
+    ]
+  }
+
+}
---
+++ src/app/model/product.ts
@@ -0,0 +1,9 @@
+export class Product{
+    id: number;
+    title : string;
+    descreption: string;
+    price: number;
+    quantity: number;
+    picture: string;
+    like: number;
+}
